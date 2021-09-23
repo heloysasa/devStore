fea80d6f8500644c726925a4a0f002ceabf2119f
heloysa <santos.heloysaarruda>
2021-09-23 17:05:18 -0300

    subindo netlify

--- site/src/components/cabecalho/index.js
+++ site/src/components/cabecalho/index.js
@@ -5,21 +5,21 @@ export default function index(){
         <Container>
              <div class="cabecalho2">
             <div class="imgs">
-                <div class="imagem"><button><img src="/assets/images/img-pro.svg"/></button></div>
-                <div class="bolinha"><img src="/assets/images/not3.svg"/></div>
+                <div class="imagem"><button><img src="/assets/images/img-pro.svg" alt=""/></button></div>
+                <div class="bolinha"><img src="/assets/images/not3.svg" alt=""/></div>
                 <div class="n">3</div>
             </div>
             <div class="frase">Olá, <b>Bruno de Oliveira</b></div>
             <div class="opcoes">
                 <div class="recarregar">
                     <button>
-                        <img src="/assets/images/recarregar.svg"/>
+                        <img src="/assets/images/recarregar.svg" alt=""/>
                     </button>
                 </div>
 
                 <div class="sair">
                     <button>
-                        <img src="/assets/images/botSair.svg"/>
+                        <img src="/assets/images/botSair.svg" alt=""/>
                     </button>
                 </div>
                 
--- site/src/components/menu/index.js
+++ site/src/components/menu/index.js
@@ -5,16 +5,16 @@ export default function index() {
         <ContainerMenu>
             <div class="lado-esquerdo">
         <div class="cabecalho">
-            <div class="logo"><img src="/assets/images/logo.svg"/></div>
+            <div class="logo"><img src="/assets/images/logo.svg" alt=""/></div>
             <div class="nome"><span>Dev</span>Store</div>
         </div>
         <div class="barra">:|</div>
         <div class="especificacoes">
             <div class="opcao">Gerenciamento</div>
-            <div class="setinha"><img src="/assets/images/setinha.svg"/></div>
+            <div class="setinha"><img src="/assets/images/setinha.svg" alt=""/></div>
         </div>
         <div class="al">
-            <div class="barra-azul"><img src="/assets/images/barLateral.svg"/></div>
+            <div class="barra-azul"><img src="/assets/images/barLateral.svg" alt=""/></div>
             <div class="click">Produtos</div>
         </div>
     </div>
